e478e3dac628aee3391f32257badad1d9e4cdbda
wjx115345 <13703528474@163.com>
2020-08-02 21:11:29 +0800

    2020-8-2 21:11

--- Vue/vue_demo1/src/views/Index.vue
+++ Vue/vue_demo1/src/views/Index.vue
@@ -8,20 +8,7 @@
 export default {
     name: 'index',
     mounted () {
-        //字符串
-        let str1 = 'http://www.baidu.com';
-        if (str1.startsWith('http://')) {
-            console.log('普通网址');
-        } else {
-            console.log('其他网址');
-        };
-
-        let str2 = '.jpg';
-        if (str2.endsWith('.jpg')) {
-            console.log('jpg文件');
-        } else {
-            console.log('其他文件');
-        }
+        
     }
 }
 </script>
