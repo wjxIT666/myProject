b3ac972718e2184fbf215d592349c9a04994c0f9
wjx115345 <13703528474@163.com>
2020-08-02 19:57:14 +0800

    2020-8-2 19:57

---
+++ Vue/vue_demo1/src/views/Index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="index">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'index',
+    mounted () {
+        //默认参数
+        let fn = (a, b = 1, c = 2) => {
+            console.log(a);
+            console.log(b);
+            console.log(c);
+        };
+        fn(0);
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
